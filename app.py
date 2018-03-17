4e308e491b81728f3764e8199aedf2bb727129b8
colinricardo <s1244995@sms.ed.ac.uk>
2018-03-17 17:05:57 +0000

    add cors

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 from flask import Flask, request
+from flask_cors import CORS
 from converter import markdown
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def index():
-  url = request.args.get('url')
+    url = request.args.get('url')
 
-  if url:
-    try:
-      content = markdown(url)
-    except Exception as e:
-      print('ERROR --> ', e)
-      return 'An error occurred with parser', 502
+    if url:
+        try:
+            content = markdown(url)
+        except Exception as e:
+            print('ERROR --> ', e)
+            return 'An error occurred with parser', 502
 
-    if content:
-      return content, 200, {'Content-Type': 'text/x-markdown; charset=UTF-8'}
+        if content:
+            return content, 200, {'Content-Type': 'text/x-markdown; charset=UTF-8'}
+        else:
+            return '404 Not Found', 404
     else:
-      return '404 Not Found', 404
-  else:
-    return 'Please give a valid url', 501
+        return 'Please give a valid url', 501
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
